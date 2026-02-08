9a8eff8a85c1cc470e34022c15af81fe5c34583d
KirillDemyanenko <demyanenkokg@gmail.com>
2026-02-08 14:48:56 +0300

    feat: add converter for miles

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from config import BOT_TOKEN
 import telebot
 
+from utils import convert_miles_to_km
+
 
 def main():
 
@@ -13,6 +15,19 @@ def main():
     @bot.message_handler(commands=['start'])
     def handle_start(message):
         bot.send_message(message.chat.id, "Привет! Я бот который умеет конвертировать все!")
+
+    @bot.message_handler(commands=['help'])
+    def handle_help(message):
+        bot.send_message(message.chat.id, "Вот список команд, которые я знаю:\n/start\n/help\n/miles_to_km")
+
+    @bot.message_handler(commands=['miles_to_km'])
+    def handle_miles_to_km(message: telebot.types.Message):
+        _, miles = message.text.split()
+        try:
+            miles = float(miles)
+            bot.send_message(message.chat.id, f"{miles} миль = {convert_miles_to_km(miles)} километров")
+        except ValueError:
+            bot.send_message(message.chat.id, "Неверно введены данные.")
 
     bot.infinity_polling()
 
---
+++ utils.py
@@ -0,0 +1,2 @@
+def convert_miles_to_km(miles: float) -> float:
+    return miles * 1.60934
